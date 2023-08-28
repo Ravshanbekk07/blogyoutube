fa45a51a6bf9c9f326bea80f27cd41bb25876cee
Ravshanbekk07 <ramazonof07@gmail.com>
2023-08-28 10:05:27 +0500

    cool

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from .serializers import BlogSerializer
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from .models import Blog
-
+from rest_framework import permissions
 
 class PostList(ListCreateAPIView):
+    permission_classes=(permissions.IsAuthenticated)
     queryset=Blog.objects.all()
     serializer_class=BlogSerializer
 class PostDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes=(permissions.IsAuthenticated)
     queryset=Blog.objects.all()
     serializer_class=BlogSerializer
